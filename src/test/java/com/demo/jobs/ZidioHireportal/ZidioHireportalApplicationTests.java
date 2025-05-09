b63480b496b45dcba0f5fae07126d9f8ac0e51fb
prashanth <munja.prashanth09@gmail.com>
2025-05-09 18:40:48 +0530

    intial commit

---
+++ src/test/java/com/demo/jobs/ZidioHireportal/ZidioHireportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jobs.ZidioHireportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZidioHireportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
